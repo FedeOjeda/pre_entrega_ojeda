58e5e12730c637d63f436e083e17377f2dd7c7b6
Fede <ojedaf45@gmail.com>
2025-10-22 16:12:41 -0300

    utils.py y conftest.py

--- Utilites/utils.py
+++ Utilites/utils.py
@@ -1,11 +1,18 @@
 from selenium.webdriver.common.by import By
 import time
 
+# Función principal Login 
 def login(driver):
+    # Accedo a la página
     driver.get("https://www.saucedemo.com/")
     
+    # Cargar el usuario
     driver.find_element(By.ID,"user-name").send_keys("standard_user")
+    
+    # Cargar la contraseña
     driver.find_element(By.ID,"password").send_keys("secret_sauce")
+    
+    # Click sobre el boton para hacer el login
     driver.find_element(By.ID, "login-button").click()
 
     time.sleep(2)
--- conftest.py
+++ conftest.py
@@ -2,6 +2,9 @@ import pytest
 from selenium import webdriver
 from Utilites.utils import login
 
+# Archivo Global
+
+# Funciones para abrir y cerrar el navegador
 @pytest.fixture
 def driver():
     driver = webdriver.Chrome()
